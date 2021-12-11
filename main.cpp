3cac925bb2d3a0056c399f0a516c1c347d20fe72
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-12-11 17:11:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
-}
+
+int main() {
+    int posizione;
+    char a[10][20], confronto[1][20];
+    for (int i=0; i<10; i++) {
+        cin >> a[i];
+
+    }
+    cin>> confronto[0];
+
+    if (funzione(a,confronto,posizione)){
+        cout << posizione << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+    return 0;}
