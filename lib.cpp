45c6c44bdd0cf527f954e4c9a005286b003ed259
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-12-11 17:12:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by tomma on 10/12/2021.
+//
 
+#include "lib.h"
+#include <cstring>
+
+bool funzione(char matrice [10][20], char nome [1][20], int &pos){
+    for (int i=0; i<10; i++){
+        if (strcmp(matrice [i], nome[0])==0){
+            pos=i;
+            return true;
+        }
+    }
+    return false;
+}
