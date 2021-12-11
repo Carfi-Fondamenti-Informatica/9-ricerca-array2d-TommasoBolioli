459f96e1cdd82d4b91c7c0ecf688c25eeaacd778
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-12-11 17:12:21 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by tomma on 10/12/2021.
+//
+
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+
+bool funzione (char matrice [10][20], char nome [1][20], int &pos);
+
+#endif //ESERCITAZIONE_9_LIB_H
 
